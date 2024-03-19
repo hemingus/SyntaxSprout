1c506c26aaceb36d983a3614557b909660f5dcc3
Heming Hanevik <heming.hanevik@gmail.com>
2024-03-19 23:58:23 +0100

    base syntax tree with branches

--- syntax-sprout/src/components/SyntaxTreeCanvas/SyntaxTreeCanvas.tsx
+++ syntax-sprout/src/components/SyntaxTreeCanvas/SyntaxTreeCanvas.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useLayoutEffect } from 'react'
 import { TreeNode } from '../SyntaxTreeNode/SyntaxTreeNode'
 import SyntaxTreeNode from '../SyntaxTreeNode/SyntaxTreeNode'
 import SyntaxTreePage from '../SyntaxTreePage/SyntaxTreePage'
@@ -10,31 +10,98 @@ interface SyntaxTreeCanvasProps {
 
 const SyntaxTreeCanvas : React.FC<SyntaxTreeCanvasProps> = ({root}) => {
     const [confirmed, setConfirmed] = useState(true)
+    const [rootNode, setRootNode] = useState(root)
+    const [lines, setLines] = useState<JSX.Element[]>([])
     
-    // const wordBlocks = () => {
-    //     return (
-    //         <div>
-    //             <ul className="wordBlock-container">
-    //                 {words.map((word, index) => (
-    //                     <li className="wordBlock" key={index}>{word}</li>
-    //                 ))}
-    //             </ul>
-    //             <button onClick={() => {setConfirmed(false)}}>Reset Sentence</button>
-    //         </div>
-    //     )
-    // }
+    const customRoot: TreeNode = {
+        label: "S",
+        children: [{label: "VP",
+                    children: [
+                        {label: "they"},
+                        {label: "were"},
+                        {label: "V", 
+                            children: [{label: "driving"}]}
+                    ]},
+                    {label: "NP",
+                    children: [
+                        {label: "down"},
+                        {label: "N",
+                            children: [{label: "Art",
+                                children: [{label: "the"}]},
+                            {label: "street"}]},
+                        {label: "completely"},
+                        {label: "unaware"}
+                    ]}
+                ]
+    }
+
+    useLayoutEffect(() => {
+        setLines([])
+        const linesToRender = renderTreeLines(rootNode, null)
+        setLines(linesToRender!)
+    }, [rootNode, window.length])
+
+ 
+
+    function renderTreeLines(node: TreeNode, parentNodeRect: DOMRect | null) {
+        const newLines: JSX.Element[] = []
+
+        if (!node.children) {
+            return newLines
+        }
+
+        const parentRect = document.getElementById(node.label)?.getBoundingClientRect()!;
+        const parentCenterX = parentRect.left + parentRect.width/2  + document.getElementById("syntax-tree-container")?.scrollLeft!;
+        const parentBottomY = parentRect.top + parentRect.height;
+
+        if (parentNodeRect === null) {
+            return renderTreeLines(node, parentRect)
+        }
+        
+        node.children.forEach((child: TreeNode) => {
+            const childRect = document.getElementById(child.label)?.getBoundingClientRect()!;
+            const childCenterX = childRect.left + childRect.width / 2;
+            const childTopY = childRect.top;
+            
+            newLines.push(
+                <line
+                    key={`${node.label}-${child.label}`}
+                    x1={parentCenterX}
+                    y1={parentBottomY}
+                    x2={childCenterX}
+                    y2={childTopY}
+                    stroke="black" 
+                    strokeWidth="3"
+                    strokeOpacity="1">
+                </line>
+                )
+            newLines.push(...renderTreeLines(child, parentRect))
+        });
+
+        return newLines
+    }
 
     const syntaxTree = () => {
-        console.log(root.children)
+        console.log(rootNode.children)
         return (
-            <SyntaxTreeNode {...root} />
+            <>
+            <button onClick={() => setRootNode(customRoot)}>Test custom root</button>
+            <div className="canvas-container">
+                <div  id="syntax-tree-container" className="canvas-container">
+                    <SyntaxTreeNode {...rootNode} />  
+                </div>
+                <svg className="tree-lines">
+                    {lines}
+                </svg>      
+            </div>
+            </>
         )
     }
 
     const content = () => {
         if (confirmed) {
             return (
-                <>  
+                <>
                     {syntaxTree()}
                     <button onClick={() => setConfirmed(false)}>Change sentence</button>
                 </>
--- syntax-sprout/src/components/SyntaxTreeNode/SyntaxTreeNode.tsx
+++ syntax-sprout/src/components/SyntaxTreeNode/SyntaxTreeNode.tsx
@@ -1,4 +1,5 @@
 import './SyntaxTreeNode.css'
+import { useState } from 'react'
 
 export type TreeNode = { 
     label: string,
@@ -7,30 +8,39 @@ export type TreeNode = {
 }
 
 const SyntaxTreeNode: React.FC<TreeNode> = (nodeData) => {
+    const [newText, setNewText] = useState("") 
+    const [editing, setEditing] = useState(false)
+
+    function updateNodeLabel() {
+        nodeData.label.replace(nodeData.label, newText)
+        setEditing(false)
+    }
 
     const returnChildren = () => {
         const childrenToReturn = nodeData.children!
+
         return (
             <div className="nodeBlock-container">
-                {childrenToReturn.map((child, index) => (
+                {childrenToReturn.map((child, index) => ( 
                     <SyntaxTreeNode key={index+child.label} {...child} />
                 ))}
             </div>
         )
     }
 
-    if (nodeData.children) {
-        return (
-            <>
-                <p className="nodeBlock">{nodeData.label}</p>
-                {returnChildren()}
-            </>
-        ) 
-        
-    }
-
-    return (
-            <p className="nodeBlock">{nodeData.label}</p>
+    if (nodeData.children) return (
+        <div className="nodeBlock-container-vertical">
+            
+            {editing ? <input type="text" onChange={(e) => setNewText(e.currentTarget.value)} onBlur={() => updateNodeLabel()}/> 
+            : <span id={nodeData.label} onClick={() => setEditing(true)} className="nodeBlock">{nodeData.label}</span>}
+            {nodeData.children ? returnChildren() : <></>}
+        </div>
+    ) 
+    else return (
+        <div className="wordBlock-container">
+            <span id={nodeData.label} className="wordBlock">{nodeData.label}</span>
+    
+        </div>
     )
 }
 
