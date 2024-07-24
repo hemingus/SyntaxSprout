46181b1359987a82f95eaa452f6b3143cd73e70f
Heming Hanevik <heming.hanevik@gmail.com>
2024-07-24 23:49:14 +0200

    moved assets folder to public

--- syntax-sprout/src/components/MainMenu/MainMenu.tsx
+++ syntax-sprout/src/components/MainMenu/MainMenu.tsx
@@ -15,14 +15,15 @@ const MainMenu : React.FC = () => {
     }
 
     return (
-        <div className="main-menu">
-            <div className="flex flex-col items-end">
-                <label className="h-6">Generate syntax tree from a sentence ⏵ </label>
-                <label className="h-6">Freely generate a tree model ⏵ </label>    
+        <div className="main-menu text-xl">
+            <div className="flex flex-row justify-center items-center">
+                <label className="text-xl">Generate syntax tree from a sentence ⏵ </label>
+                <button className="text-xl bg-green-900 hover:text-white text-slate-300 cursor-pointer" onClick={() => setShowSyntaxTree(true)}>Syntax Tree Generator</button>
+                    
             </div>
-            <div className="flex flex-col items-start">
-                <button className="h-6" onClick={() => setShowSyntaxTree(true)}>Syntax Tree Generator</button>
-                <button className="h-6">Custom Tree Generator</button>
+            <div className="flex flex-row justify-center items-center">
+                <label className="text-xl">Freely generate a tree model ⏵ </label>
+                <button className="text-xl bg-green-900 hover:text-white text-slate-300 cursor-pointer">Custom Tree Generator</button>
             </div>
         </div>
     )
