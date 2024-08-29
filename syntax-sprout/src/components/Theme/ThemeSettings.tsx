986de534178a43f5e91c0ffdc12f4924df9c98e4
Heming Hanevik <heming.hanevik@gmail.com>
2024-08-29 19:56:14 +0200

    ThemeSettings refactor and styling

--- syntax-sprout/src/components/Theme/ThemeSettings.tsx
+++ syntax-sprout/src/components/Theme/ThemeSettings.tsx
@@ -8,7 +8,15 @@ const ThemeSettings = () => {
         <div>
             <ul>
                 {Themes.map((theme: Theme) => (
-                    <li onClick={() => setTheme(theme)} className="solid bg-white text-black cursor-pointer hover:bg-lime-500" key={theme.name}>{theme.name}</li>
+                <li
+                    onClick={() => setTheme(theme)} 
+                    className="solid p-2 m-0 rounded list-none bg-gradient-to-b from-slate-800 to-slate-500
+                     text-white 
+                     cursor-pointer 
+                     hover:bg-gradient-to-b hover:from-slate-700 hover:to-lime-500"
+                    key={theme.name}>
+                        {theme.name}
+                </li>
                 ))}
             </ul>
         </div>
