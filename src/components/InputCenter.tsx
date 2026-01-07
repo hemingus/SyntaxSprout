0625eec69832a37c98d0fcecec2f3a082021389a
hemingus <heming.hanevik@gmail.com>
2026-01-07 14:33:38 +0100

    add optional text property

--- src/components/InputCenter.tsx
+++ src/components/InputCenter.tsx
@@ -2,17 +2,18 @@ import { useState, useEffect, useRef } from 'react'
 
 interface InputCenterProps {
     label: string
-    placeholder: string
+    text?: string
+    placeholder?: string
     isVisible: boolean
     onConfirm: (text: string) => void
     onCancel: () => void
   }
   
-const InputCenter: React.FC<InputCenterProps> = ({ label, placeholder, isVisible, onConfirm, onCancel }) => {
+const InputCenter: React.FC<InputCenterProps> = ({ label, text, placeholder, isVisible, onConfirm, onCancel }) => {
     if (!isVisible) return null
 
     const inputRef = useRef<HTMLInputElement>(null)
-    const [newNodeText, setNewNodeText] = useState("")
+    const [newNodeText, setNewNodeText] = useState(text || "")
 
     useEffect(() => {
         if (inputRef.current) inputRef.current.focus()
@@ -30,12 +31,13 @@ const InputCenter: React.FC<InputCenterProps> = ({ label, placeholder, isVisible
     return (
         <div className="fixed inset-0 w-full h-full flex flex-col justify-center items-center z-50 text-white text-4xl bg-black/50"
             onClick={() => onCancel()}>
-            <div className="flex flex-col justify-center items-center p-4 bg-slate-600 border-solid border-gray-400 rounded-2xl gap-2.5">
+            <div className="flex flex-col justify-center items-center p-4 bg-slate-600 border-solid border-gray-400 rounded-2xl gap-2.5 z-100"
+                onClick={(e) => {e.stopPropagation()}}>
             <label>{label}</label>
             <input className="w-auto max-w-[80vw] bg-black text-sky-300 text-4xl"
                 autoComplete="off"
                 spellCheck="false"
-                placeholder={placeholder}
+                placeholder={placeholder || ""}
                 ref={inputRef}
                 value={newNodeText}
                 onChange={(e) => setNewNodeText(e.currentTarget.value)}
