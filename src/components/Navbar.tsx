b27289ecb8e250c61e685d09734035c7f6615d4e
hemingus <heming.hanevik@gmail.com>
2026-01-12 17:34:35 +0100

    make navbare responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,26 +2,26 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-center items-center top-0 left-0 w-full gap-5 bg-gradient-to-b from-transparent to-black">
-        <Link className="no-underline" 
+        <nav className="flex flex-col xs:flex-row justify-center items-center top-0 left-0 w-full gap-0 xs:gap-5 bg-gradient-to-b from-transparent to-black">
+        <Link className="no-underline w-3/4 xs:w-auto text-center"
             to="/dashboard">
-            <h2 className="p-2 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black 
+            <h2 className="p-2 m-0 xs:m-3 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black 
             hover:from-green-600 hover:to-black">
                 Dashboard
             </h2>
         </Link>
 
-        <Link className="no-underline" 
+        <Link style={{padding: "0", margin: "0"}} className="no-underline w-3/4 xs:w-auto text-center" 
             to="/tutorial">
-            <h2 className="p-2 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black
+            <h2 className="p-2 m-0 xs:m-3 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black
             hover:from-green-600 hover:to-black">
                 Tutorial
             </h2>
         </Link>
 
-        <Link className="no-underline" 
+        <Link className="no-underline w-3/4 xs:w-auto text-center" 
             to="/about">
-            <h2 className="p-2 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black
+            <h2 className="p-2 m-0 xs:m-3 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black
             hover:from-green-600 hover:to-black">
                 About
             </h2>
