07dece5117a360afb91ff68ea7479a4ee8926907
Heming Hanevik <heming.hanevik@gmail.com>
2026-01-13 08:34:07 +0100

    tweak responsiveness

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -5,21 +5,21 @@ import StartIcon from "../../public/assets/icons/start.svg?react"
 const MainMenu : React.FC = () => {
 
     return (
-        <main className="flex flex-col bg-gradient-to-b from-black to-slate-700 items-center justify-center">
-            <h1 className="text-[clamp(1.5rem,5vw,3rem)] font-extrabold bg-gradient-to-r from-canvas-brown to-canvas-green bg-clip-text text-transparent">Welcome to Syntax Sprout</h1>
-            <p className="text-[clamp(0.8rem,2.5vw,1.5rem)] text-center text-white">
+        <main className="flex flex-col gap-4 bg-gradient-to-b from-black to-slate-700 items-center justify-center p-4">
+            <h1 className="text-center text-[clamp(2rem,5vw,3rem)] font-extrabold bg-gradient-to-r from-canvas-brown via-canvas-green to-green-400 bg-clip-text text-transparent">Welcome to SyntaxSprout</h1>
+            <p className="text-[clamp(1.2rem,2.5vw,1.5rem)] text-center text-white">
                 If you are analyzing sentences and are looking to construct a syntax tree, <br/>
                 here is a neat tool to help you accomplish that!
             </p>
             <div className="flex flex-wrap justify-center items-center gap-8">
                 <Link className="no-underline inline-block" to="/tutorial">
-                    <div className="flex items-center w-fit px-[clamp(4px,4vw,16px)] py-[clamp(4px,vw,8px)] bg-[rgba(0,0,0,0.5)] gap-4 text-white font-medium text-[clamp(1rem,2vw,1.5rem)] rounded-xl border-solid border-[rgba(200,200,200,0.5)] border-[1px] hover:bg-gray-900 hover:border-white">
+                    <div className="flex items-center w-fit px-[clamp(4px,4vw,16px)] py-[clamp(4px,vw,8px)] bg-[rgba(0,0,0,0.5)] gap-4 text-white font-medium text-[clamp(1.2rem,3vw,1.5rem)] rounded-xl border-solid border-[rgba(200,200,200,0.5)] border-[1px] hover:bg-gray-900 hover:border-white">
                         <TutorialIcon className="h-[clamp(2rem,4vw,3rem)] w-auto text-blue-500"/>
                         <p>Tutorial</p>
                     </div>
                 </Link>
                 <Link className="no-underline inline-block" to="/dashboard">
-                    <div className="flex items-center w-fit px-[clamp(4px,4vw,16px)] py-[clamp(4px,vw,8px)] bg-[rgba(0,0,0,0.5)] gap-4 text-white font-medium text-[clamp(1rem,2vw,1.5rem)] rounded-xl border-solid border-[rgba(200,200,200,0.5)] border-[1px] hover:bg-gray-900 hover:border-white">
+                    <div className="flex items-center w-fit px-[clamp(4px,4vw,16px)] py-[clamp(4px,vw,8px)] bg-[rgba(0,0,0,0.5)] gap-4 text-white font-medium text-[clamp(1.2rem,3vw,1.5rem)] rounded-xl border-solid border-[rgba(200,200,200,0.5)] border-[1px] hover:bg-gray-900 hover:border-white">
                         <StartIcon className="h-[clamp(2rem,4vw,3rem)] w-auto text-lime-500"/>
                         <p>Start building</p>
                     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,29 +2,65 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-col xs:flex-row justify-center items-center top-0 left-0 w-full gap-0 xs:gap-5 bg-gradient-to-b from-transparent to-black">
-        <Link className="no-underline w-3/4 xs:w-auto text-center"
-            to="/dashboard">
-            <h2 className="p-2 m-0 xs:m-3 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black 
-            hover:from-green-600 hover:to-black">
-                Dashboard
-            </h2>
+        <nav className="flex justify-center items-center top-0 left-0 w-full gap-5 p-1 bg-gradient-to-b from-transparent to-black">
+        <Link
+            to="/dashboard"
+            className="
+                no-underline
+                text-center
+                text-md
+                xs:text-2xl
+                font-bold
+                p-2
+                m-1 xs:m-3
+                rounded-xl
+                text-lime-500
+                bg-gradient-to-b from-green-800 to-black
+                border-solid border-[1px] border-black
+                hover:from-green-600 hover:to-black
+            "
+        >
+            Dashboard
         </Link>
 
-        <Link style={{padding: "0", margin: "0"}} className="no-underline w-3/4 xs:w-auto text-center" 
-            to="/tutorial">
-            <h2 className="p-2 m-0 xs:m-3 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black
-            hover:from-green-600 hover:to-black">
-                Tutorial
-            </h2>
+        <Link
+            to="/tutorial"
+            className="
+                no-underline
+                text-center
+                text-md
+                xs:text-2xl
+                font-bold
+                p-2
+                m-1 xs:m-3
+                rounded-xl
+                text-lime-500
+                bg-gradient-to-b from-green-800 to-black
+                border-solid border-[1px] border-black
+                hover:from-green-600 hover:to-black
+            "
+        >
+            Tutorial
         </Link>
 
-        <Link className="no-underline w-3/4 xs:w-auto text-center" 
-            to="/about">
-            <h2 className="p-2 m-0 xs:m-3 rounded-xl text-lime-500 bg-gradient-to-b from-green-800 to-black border-solid border-black
-            hover:from-green-600 hover:to-black">
-                About
-            </h2>
+        <Link
+            to="/about"
+            className="
+                no-underline
+                text-center
+                text-md
+                xs:text-2xl
+                font-bold
+                p-2
+                m-1 xs:m-3
+                rounded-xl
+                text-lime-500
+                bg-gradient-to-b from-green-800 to-black
+                border-solid border-[1px] border-black
+                hover:from-green-600 hover:to-black
+            "
+        >
+            About
         </Link>
     </nav>
     )
