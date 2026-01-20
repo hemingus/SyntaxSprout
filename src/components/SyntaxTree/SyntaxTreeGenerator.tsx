5e27e1c26e66aebc7e5421c1bc3bb40457dc5c0a
hemingus <heming.hanevik@gmail.com>
2026-01-20 14:17:23 +0100

    update syntax tree generator styles

--- src/components/SyntaxTree/SyntaxTreeGenerator.tsx
+++ src/components/SyntaxTree/SyntaxTreeGenerator.tsx
@@ -71,18 +71,18 @@ useEffect(() => {
             <div className="fixed inset-0 flex flex-col items-center justify-start z-50 bg-black/50">
                 <h1 className="text-center text-white bg-black rounded-full w-fit p-2 shadow-[0_0_50px_1px_white]">New Tree</h1>
                 <div className="flex flex-col justify-center items-center p-5 w-fit relative rounded-xl
-                lg:w-1/2 md:w-3/4 bg-gradient-to-tr from-green-700 via-emerald-900 to-green-700
-                border-solid border-4 border-slate-700">
+                w-full lg:w-1/2 md:w-3/4 bg-gradient-to-b from-emerald-700 to-black
+                border-solid border-2 border-emerald-950">
                     <div className="w-full m-2 flex flex-col justify-center items-center gap-4">
                         <label className="text-xl text-slate-300 font-bold">Tree Name: </label>
-                        <h3 className="m-0 w-full p-1 text-yellow-400 bg-gray-700 text-2xl text-center">{treeName}</h3>
+                        <h3 className="rounded m-0 w-full p-1 text-yellow-400 bg-gray-800 text-2xl text-center">{treeName}</h3>
                         <label className="text-xl text-slate-300 font-bold">Root Label: </label>
-                        <h3 className="m-0 w-full p-1 text-emerald-400 bg-gray-700 text-2xl text-center">{rootLabel}</h3>
+                        <h3 className="rounded m-0 w-full p-1 text-emerald-400 bg-gray-800 text-2xl text-center">{rootLabel}</h3>
                         <label className="text-xl text-slate-300 font-bold">Sentence: </label>
-                        <h3 className="m-0 w-full p-1 text-lime-400 bg-gray-700 text-2xl text-center">{sentence}</h3>
+                        <h3 className="rounded m-0 w-full p-1 text-lime-400 bg-gray-800 text-2xl text-center">{sentence}</h3>
                     </div>
                     <div className="w-full m-2 flex flex-col gap-4">
-                        <input className="w-full font-sans text-center text-2xl text-white bg-black"
+                        <input className="w-full font-sans text-center text-2xl text-white bg-black rounded-md"
                             ref={inputTreeNameRef}
                             autoComplete="off"
                             spellCheck="false"
@@ -91,7 +91,7 @@ useEffect(() => {
                             onKeyDown={handleKeyDown}
                         />
                         
-                        <input className="w-full font-sans text-center text-2xl text-white bg-black"
+                        <input className="w-full font-sans text-center text-2xl text-white bg-black rounded-md"
                             ref={inputRootLabelRef}
                             autoComplete="off"
                             spellCheck="false"
@@ -100,7 +100,7 @@ useEffect(() => {
                             onKeyDown={handleKeyDown}
                         />
                     
-                        <input className="w-full font-sans text-center text-2xl text-white bg-black"
+                        <input className="w-full font-sans text-center text-2xl text-white bg-black rounded-md"
                             ref={inputSentenceRef}
                             autoComplete="off"
                             spellCheck="false"
@@ -110,12 +110,12 @@ useEffect(() => {
                             onChange={(e) => setSentence(e.target.value)}  
                         />
                     </div>
-                    <button className="mt-4 p-1 text-xl text-gray-300 bg-slate-800 cursor-pointer 
-                    hover:text-white hover:bg-slate-700 hover:shadow-[0_0_20px_1px_white]"
+                    <button className="mt-4 p-1 text-xl text-gray-300 bg-slate-800 cursor-pointer rounded-lg 
+                    hover:text-emerald-300 hover:bg-slate-700 hover:shadow-[0_0_10px_1px_seagreen]"
                         onClick={() => {generateSyntaxTree(); onCancel}}>
                         Generate Syntax Tree
                     </button>
-                    <button className="mt-8 text-xl text-gray-400 bg-slate-800 cursor-pointer 
+                    <button className="mt-8 text-xl text-gray-400 bg-slate-800 cursor-pointer rounded-md
                     hover:text-gray-300 hover:bg-slate-700"
                         onClick={onCancel}>
                         Cancel
